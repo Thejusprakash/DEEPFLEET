4457881af17bf375704b01ca4cce3459950a0f87
Thejusprakash <thejusprakash03#gmail.com>
2023-12-21 01:31:31 +0530

    Updates

---
+++ precache-manifest.2396271338b38c38b3b34e201659cbde.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592f06683a0ad0ccf884ba804cf9b28c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd4d91c782a24b9bc1ba",
+    "url": "/static/css/main.a2300714.chunk.css"
+  },
+  {
+    "revision": "ca28dcabc2188bca3778",
+    "url": "/static/js/2.244d80c1.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/2.244d80c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3dd2727a0a8c37eab9a",
+    "url": "/static/js/3.d00dc11d.chunk.js"
+  },
+  {
+    "revision": "1ae8fab53ee1fa3d877a4bf74faef1e8",
+    "url": "/static/js/3.d00dc11d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c656cc19f3bbc27ae64",
+    "url": "/static/js/4.0f3d2ec6.chunk.js"
+  },
+  {
+    "revision": "a76e873f1b2326391d665076330da62d",
+    "url": "/static/js/4.0f3d2ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f8361e0b10844fd271",
+    "url": "/static/js/5.3a5b65a4.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "/static/js/5.3a5b65a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4d91c782a24b9bc1ba",
+    "url": "/static/js/main.fd8c68f3.chunk.js"
+  },
+  {
+    "revision": "de78e9db5fa2a6e37d88",
+    "url": "/static/js/runtime-main.1b592e99.js"
+  }
+]);
